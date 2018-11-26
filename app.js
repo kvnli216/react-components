0ac20ca12b1c3e7453399e61d1690a2df0662ab1
Kevin Li <li.ctrl.z@gmail.com>
2018-11-26 12:17:26 -0800

    implement grocerylist with react compements and props

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // TODO
+let GroceryList = (props) => (
+  <ul>
+    <li>{props.list[0]}</li>
+    <li>{props.list[1]}</li>
+  </ul>
+);
+
+let GroceryListItem = <GroceryList list={['rice', 'beef chuck']}/>;
+
+ReactDOM.render(GroceryListItem, document.getElementById('app'));
+
+
+// let Rice = () => (
+//     <div>rice</div>
+// );
+
+// let BeefChuck = () => (
+//   <div>beef chuck</div>
+// );
